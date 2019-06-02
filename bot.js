9e45116014afe120635e0f2d44edbd273c2821e4
jackSkul <51283453+jackSkul@users.noreply.github.com>
2019-06-02 00:58:30 -0600

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+const fs = require('fs')
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+fs.readdir('./events/', (err, files) => {
+  files.forEach(file => {
+    const eventHandler = require(`./events/${file}`)
+    const eventName = file.split('.')[0]
+    client.on(eventName, (...args) => eventHandler(client, ...args))
+  })
+})
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+  if (msg.content === 'bark') {
+    msg.reply('woof!')
+  }
+})
+
+client.on('guildMemberAdd', member => {
+  member.send(
+    `Welcome to the server! Please be aware that we won't tolerate troll, spam or harassment. Have fun 😀`
+  )
+})
+
+client.on('message', message => {
+  if (message.content.startsWith('!kick')) {
+    const member = message.mentions.members.first()
+
+    if (!member) {
+      return message.reply(
+        `Who are you trying to kick? You must mention a user.`
+      )
+    }
+
+    if (!member.kickable) {
+      return message.reply(`I can't kick this user. Sorry!`)
+    }
+
+    return member
+      .kick()
+      .then(() => message.reply(`${member.user.tag} was kicked.`))
+      .catch(error => message.reply(`Sorry, an error occured.`))
+  }
+})
+
+client.login(process.env.BOT_TOKEN)
